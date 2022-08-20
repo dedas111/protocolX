d42f75a5598bdfbf8ee2ba2bfc61a5c2247bfb46
Debajyoti Das <cs09b009@iith.ac.in>
2022-08-20 11:47:10 +0200

    sjfkh

--- server/server_test.go
+++ server/server_test.go
@@ -68,7 +68,7 @@ const (
 	testDatabase       = "testDatabase.db"
 	remoteIP           = "54.92.157.34" // remote IP of compute for testing - not needed for standalone test because it uses multiple compute nodes
 	localIP            = "34.207.125.78" // IP of the client receiving the packets for the tests
-	threadsCountClient = 8                // listener threads on client
+	threadsCountClient = 16                // listener threads on client
 	threadsCountServer = 16                // listener threads on compute/server
 )
 
